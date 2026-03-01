9ba5b66b38e78dc5ec720ab9777ac3d273582106
zackkingzone-png <zackkingzone@gmail.com>
2026-03-02 04:31:24 +0800

    Enable wall check in aimbot settings

--- aimbot.lua
+++ aimbot.lua
@@ -98,7 +98,7 @@ getgenv().ExunysDeveloperAimbot = {
 
 		TeamCheck = true,
 		AliveCheck = true,
-		WallCheck = false,
+		WallCheck = true,
 
 		OffsetToMoveDirection = false,
 		OffsetIncrement = 15,
